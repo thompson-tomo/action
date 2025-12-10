5be6ab068da3f2482c8639f1f4fedc0bd6147ee8
James Thompson <thompson.tomo@outlook.com>
2025-12-11 00:20:27 +1100

    add in rollup to bundle files

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const core = require('@actions/core');
+const spawn = require('child_process').spawn;
+const path = require('path');
+
+const args = [
+    ...core.getInput("config").trim().split(' ').map(value => `--config=${value}`),
+    `--workdir=${core.getInput("workdir")}`,
+    `--error-output-format=${core.getInput("error-output-format")}`,
+    `--debug=${core.getInput("debug")}`,
+    `--warn=${core.getInput("warn")}`,
+    ...core.getInput("paths").trim().split(' ')
+];
+
+const child = spawn(
+    path.join(__dirname, "../node_modules/@ls-lint/ls-lint/bin/cli.js"),
+    args.filter(Boolean),
+    {stdio: 'inherit'}
+);
+
+child.on('close', function (code) {
+    if (code !== 0) {
+        process.exit(1);
+    }
+});
